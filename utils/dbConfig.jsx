1df098db4599cb4a1d70c8aa5d9f45b13588c664
Varad J <varad.gcs@gmail.com>
2025-04-13 21:38:12 +0530

    Update dbConfig.jsx

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://finan-smart_owner:uk3aed9QZotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/finan-smart?sslmode=require"
+  "postgresql://neondb_owner:npg_nB0rvGlotHb3@ep-fancy-queen-a1zuzon4-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 );
 export const db = drizzle(sql, { schema });
